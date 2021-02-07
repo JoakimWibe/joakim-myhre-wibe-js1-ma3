6f39a9800fb9b79bb3fe1d72f2159b810c2ed0f2
JoakimWibe <71314249+JoakimWibe@users.noreply.github.com>
2021-02-07 21:34:24 +0100

    Update question-2.js

--- js/question-2.js
+++ js/question-2.js
@@ -1,32 +1,23 @@
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
-
 const resultsContainer = document.querySelector(".results");
 
 async function getGames() {
-
     try {
         const response = await fetch(url);
-
         const data = await response.json();
-
         const results = data.results
 
         resultsContainer.innerHTML = "";
 
-        console.log(results)
-
         for (let i = 0; i < results.length; i++) {
-
             if (i === 8) {
                 break;
             }
-
-            resultsContainer.innerHTML += `<div class="result">${"NAME: " + results[i].name + " " + "RATING: " + results[i].rating + " " + "NUMBER OF TAGS: "}</div>`;
+            resultsContainer.innerHTML += `<div class="result">${"NAME: " + results[i].name + " " + "RATING: " + results[i].rating + " " + "NUMBER OF TAGS: " + results[i].tags.length}</div>`;
         }
     } catch (error) {
         resultsContainer.innerHTML = error;
     }
-    
 }
 
 getGames();
